e1d12de6efb9c07c9cd7ab1d268ebd9f5738e4ef
DotaKobayashi <dota.kobayashi@gmail.com>
2014-04-29 17:26:47 +0900

    Initial Commit

---
+++ SingletonTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingletonTest
+//
+//  Created by 小林堂太 on 2014/04/29.
+//  Copyright (c) 2014年 dota.kobayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingletonTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SingletonTest
+//
+//  Created by 小林堂太 on 2014/04/29.
+//  Copyright (c) 2014年 dota.kobayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
